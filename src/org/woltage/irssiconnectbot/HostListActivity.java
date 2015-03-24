a26c7b7c8b4682d8fb5d1086290a7077cac38e41
m0viefreak <m0viefreak.cm@googlemail.com>
2015-03-24 06:18:04 +0100

    HostList: fix spinner layout
    Use the same layout for the item and the dropdown menu.

--- src/org/woltage/irssiconnectbot/HostListActivity.java
+++ src/org/woltage/irssiconnectbot/HostListActivity.java
@@ -231,7 +231,7 @@ public class HostListActivity extends ListActivity {
 		transportSpinner = (Spinner)findViewById(R.id.transport_selection);
 		transportSpinner.setVisibility(makingShortcut ? View.GONE : View.VISIBLE);
 		ArrayAdapter<String> transportSelection = new ArrayAdapter<String>(this,
-				android.R.layout.simple_spinner_item, TransportFactory.getTransportNames());
+				android.R.layout.simple_spinner_dropdown_item, TransportFactory.getTransportNames());
 		transportSelection.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		transportSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 			public void onItemSelected(AdapterView<?> arg0, View view, int position, long id) {
